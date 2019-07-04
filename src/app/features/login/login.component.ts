8ec2ee78c33d5f7d65dcf9242e2e74b091322a00
kumarshan <kumaruidivloper@gmail.com>
2019-07-05 00:16:42 +0530

    Login-UI-Created

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginUserData = {};
+
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  loginUser() {
+    console.log(this.loginUserData);
   }
 
 }
