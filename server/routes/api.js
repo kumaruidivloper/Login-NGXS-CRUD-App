54ae35b912a28ea407a656f2276434d48dff6ade
kumarshan <kumaruidivloper@gmail.com>
2019-07-04 19:53:47 +0530

    Login-Routes-imp

--- server/routes/api.js
+++ server/routes/api.js
@@ -33,4 +33,23 @@ router.post('/register', (req, res) => {
     })
 })
 
+// Login Router Function
+router.post('/login', (req, res) => {
+    let userData = req.body
+
+    User.findOne({email: userData.email}, (error, user) => {
+        if (error) {
+            console.log(error)
+        } else {
+            if(!user) {
+                res.status(401).sent('Invalid Email')
+            } else if (user.password !== userData.password  ) {
+                res.status(401).send('Invalid password')
+            } else {
+                res.status(200).send(user)
+            }
+        }
+    })
+})
+
 module.exports = router;
