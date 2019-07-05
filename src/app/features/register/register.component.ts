a957df9ff4688b16e6923f502b20153317a783ab
kumarshan <kumaruidivloper@gmail.com>
2019-07-05 17:58:15 +0530

    Register-form-validation

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,23 +1,106 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { AuthService } from '../../core/services/auth.service';
+
+// @Component({
+//   selector: 'app-register',
+//   templateUrl: './register.component.html',
+//   styleUrls: ['./register.component.scss']
+// })
+// export class RegisterComponent implements OnInit {
+
+//   public registerUserData = {};
+
+//   constructor(private auth: AuthService, private router: Router) { }
+
+//   ngOnInit() {
+//   }
+
+//   registerUser() {
+//     this.auth.registerUser(this.registerUserData)
+//     .subscribe(
+//       res => {
+//         console.log(res);
+//         localStorage.setItem('token', res.token);
+//         this.router.navigate(['/customer']);
+//       },
+//       err => console.log(err)
+//     );
+//   }
+
+// }
+
+
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CustomValidators } from '../../core/validators/custom-validators';
 import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  public frmSignup: FormGroup;
 
-  public registerUserData = {};
-
-  constructor(private auth: AuthService, private router: Router) { }
-
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router) {
+    this.frmSignup = this.createSignupForm();
   }
 
-  registerUser() {
-    this.auth.registerUser(this.registerUserData)
+  createSignupForm(): FormGroup {
+    return this.fb.group(
+      {
+        email: [
+          null,
+          Validators.compose([Validators.email, Validators.required])
+        ],
+        password: [
+          null,
+          Validators.compose([
+            Validators.required,
+            // check whether the entered password has a number
+            CustomValidators.patternValidator(/\d/, {
+              hasNumber: true
+            }),
+            // check whether the entered password has upper case letter
+            CustomValidators.patternValidator(/[A-Z]/, {
+              hasCapitalCase: true
+            }),
+            // check whether the entered password has a lower case letter
+            CustomValidators.patternValidator(/[a-z]/, {
+              hasSmallCase: true
+            }),
+            // check whether the entered password has a special character
+            CustomValidators.patternValidator(
+              /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/,
+              {
+                hasSpecialCharacters: true
+              }
+            ),
+            Validators.minLength(8)
+          ])
+        ],
+        confirmPassword: [null, Validators.compose([Validators.required])]
+      },
+      {
+        // check whether our password and confirm password match
+        validator: CustomValidators.passwordMatchValidator
+      }
+    );
+  }
+
+  // submit() {
+  //   // do signup or something
+  //   console.log(this.frmSignup.value);
+  // }
+
+  signUp() {
+    this.auth.registerUser(this.frmSignup.value)
     .subscribe(
       res => {
         console.log(res);
@@ -27,5 +110,4 @@ export class RegisterComponent implements OnInit {
       err => console.log(err)
     );
   }
-
 }
