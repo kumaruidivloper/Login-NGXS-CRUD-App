179504caaed62579a631f868662f8c2b22289bd4
kumaruidivloper <kumaruidivloper@gmail.com>
2019-08-01 18:42:02 +0530

    Server-DB-Connection-String-Update

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,7 +5,8 @@ const User = require('../models/user')
 const mongoose = require('mongoose')
 
 // DB Connection String
-const db = "mongodb+srv://kumar:kumar1234@cluster0-t6xml.mongodb.net/test?retryWrites=true"
+// const db = "mongodb+srv://kumar:kumar1234@cluster0-t6xml.mongodb.net/test?retryWrites=true"
+const db = 'mongodb+srv://kumar:kumar1234@cluster0-t6xml.mongodb.net/test?retryWrites=true&w=majority'
 
 // DB Connection function
 mongoose.connect(db, err => {
